18d5f0abcc148c59df8dd3d85aaaad0abc9ea746
programmeranish <programmer.anish.shrestha@gmail.com>
2024-08-05 17:47:34 +0545

    Clear understarding

---
+++ app/f1/page.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+import React from "react";
+
+export default function page() {
+  return (
+    <div>
+      f1
+      <Link href="/f1/f2">f2</Link>
+    </div>
+  );
+}

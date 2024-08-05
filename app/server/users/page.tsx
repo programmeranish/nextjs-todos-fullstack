a180331bb962dee91a065ae020da603c8a42d8bd
programmeranish <programmer.anish.shrestha@gmail.com>
2024-08-05 17:59:35 +0545

    Fix

--- app/server/users/page.tsx
+++ app/server/users/page.tsx
@@ -5,21 +5,22 @@ type UserType = {
 };
 
 export default async function Users() {
-  const res = await fetch("https://jsonplaceholder.typicode.com/users1");
-  const data: UserType[] = await res.json();
-  await new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve("");
-    }, 2000);
-  });
-  return (
-    <div>
-      {data.map((user) => (
-        <div key={user.id} className="divide-y-2">
-          <h1>{user.name}</h1>
-          <p>{user.email}</p>
-        </div>
-      ))}
-    </div>
-  );
+  return <></>;
+  // const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  // const data: UserType[] = await res.json();
+  // await new Promise((resolve, reject) => {
+  //   setTimeout(() => {
+  //     resolve("");
+  //   }, 2000);
+  // });
+  // return (
+  //   <div>
+  //     {data.map((user) => (
+  //       <div key={user.id} className="divide-y-2">
+  //         <h1>{user.name}</h1>
+  //         <p>{user.email}</p>
+  //       </div>
+  //     ))}
+  //   </div>
+  // );
 }
